7d03940920a828869d4c32b42f6b9127cced8274
Justin Blaisdell <jblaisdell@5thcinema.com>
2012-12-06 19:38:39 -0600

    Starting new design

--- index.php
+++ index.php
@@ -1,32 +1,34 @@
 <?php include("header.php"); ?>
 
-<content class="grid col-2-3">
+<div class="grid col-2-3">
   <div class="post">
     <h2><a href="./posts.php">Post Title</a></h2>
       <div>
         blog, random and short stuff
       </div>
-    <small>posted on july 3rd, 2013 by Matt, edited August 4th by Matt</small>
+    <small>posted on July 3rd, 2013 by Matt, edited August 4th by Matt</small>
   </div>
   <div class="post">
     <h2><a href="./posts.php">Post Title</a></h2>
       <div>
         lorum ipsum on a plane, don't really have a lorum thing so I'll just type some stuff here I guess this one will be a longer one and then the next post will be shorter because some of his posts will be short and stuff but this one has to be long so I'll keep typing for a bit and then see where I am. Well this is a mighty fine day don't you think. I think so. That's why I said it. I'm on a plane.
       </div>
-    <small>posted on july 3rd, 2013 by Matt, edited August 4th by Matt</small>
+    <small>posted on July 3rd, 2013 by Matt, edited August 4th by Matt</small>
   </div>
   <div class="post">
     <h2><a href="./posts.php">Post Title</a></h2>
       <div>
         blog, random and short stuff - add a bit of lorum to just make it wrap a bit or not whatever. Actually I'll make this one a little bit longer cause hey why not you know? And wrap.
       </div>
-    <small>posted on july 3rd, 2013 by Matt, edited August 4th by Matt</small>
+    <small>posted on July 3rd, 2013 by Matt, edited August 4th by Matt</small>
   </div>
   <div>
-    <a href="#">next</a> | <a href="#">previous</a>
+    next and previous links
   </div>
-</content>
+</div>
 
 <?php include("sidebar.php"); ?>
+
+</div><!-- /end .wrapper -->
 
 <?php include("footer.php"); ?>
